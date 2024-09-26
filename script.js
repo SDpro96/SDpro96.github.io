dcf5481ca88eb677274564e82c5afe899cd52075
SDpro <66707876+SDpro96@users.noreply.github.com>
2024-09-27 02:08:19 +0630

    2

--- script.js
+++ script.js
@@ -13,6 +13,21 @@ const themes = [
         background: "#192A51",
         color: "#FFFFFF",
         primaryColor: "#967AA1"
+    },
+    {
+        background: "#F7B267",
+        color: "#000000",
+        primaryColor: "#F4845F"
+    },
+    {
+        background: "#F25F5C",
+        color: "#000000",
+        primaryColor: "#642B36"
+    },
+    {
+        background: "#231F20",
+        color: "#FFF",
+        primaryColor: "#BB4430"
     }
 ];
 
@@ -21,6 +36,7 @@ const setTheme = (theme) => {
     root.style.setProperty("--background", theme.background);
     root.style.setProperty("--color", theme.color);
     root.style.setProperty("--primary-color", theme.primaryColor);
+    root.style.setProperty("--glass-color", theme.glassColor);
 };
 
 const displayThemeButtons = () => {
